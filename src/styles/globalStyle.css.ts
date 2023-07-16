10c4dea68a58ca51afb34fae618080dabad77f33
‘lokba-pf’ <lokba.pf@kakaobrain.com>
2023-07-17 01:35:30 +0900

    style: add globalStyle

---
+++ src/styles/globalStyle.css.ts
@@ -0,0 +1,13 @@
+import { globalStyle } from "@vanilla-extract/css";
+
+globalStyle("*", {
+  boxSizing: "border-box",
+});
+
+globalStyle("ul", {
+  padding: 0,
+});
+
+globalStyle("li", {
+  listStyle: "none",
+});
